4a5596ecf3793993c3f4d1c2f4d3162a39d987e3
Kilisio Veronicah <kilisionicah@gmail.com>
2023-08-30 12:44:36 +0300

    Handle Claims with JWT

--- JWTAuthentication/Controllers/LoginController.cs
+++ JWTAuthentication/Controllers/LoginController.cs
@@ -3,13 +3,15 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 
 namespace JWTAuthentication.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class LoginController : Controller
     {
@@ -39,10 +41,16 @@ namespace JWTAuthentication.Controllers
         {
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
+            var claims = new[] {
+        new Claim(JwtRegisteredClaimNames.Sub, userInfo.Username),
+        new Claim(JwtRegisteredClaimNames.Email, userInfo.EmailAddress),
+        new Claim("DateOfJoing", userInfo.DateOfJoing.ToString("yyyy-MM-dd")),
+        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+    };
             var token = new JwtSecurityToken(_config["Jwt:Issuer"],
               _config["Jwt:Issuer"],
-              null,
+              claims,
+              
               expires: DateTime.Now.AddMinutes(120),
               signingCredentials: credentials);
 
@@ -55,11 +63,39 @@ namespace JWTAuthentication.Controllers
 
             //Validate the User Credentials
             //Demo Purpose, I have Passed HardCoded User Information
-            if (login.Username == "Jignesh")
+            if (login.Username == "vee")
             {
-                user = new UserModel { Username = "Jignesh Trivedi", EmailAddress = "test.btest@gmail.com" };
+                user = new UserModel { Username = "Vee Test", EmailAddress = "test.@gmail.com" };
             }
             return user;
+        }
+        [HttpGet]
+        [Authorize]
+        public ActionResult<IEnumerable<string>> values()
+        {
+            return new string[] { "value1", "value2", "value3", "value4", "value5" };
+        }
+        [HttpGet]
+        [Authorize]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var currentUser = HttpContext.User;
+            int spendingTimeWithCompany = 0;
+
+            if (currentUser.HasClaim(c => c.Type == "DateOfJoing"))
+            {
+                DateTime date = DateTime.Parse(currentUser.Claims.FirstOrDefault(c => c.Type == "DateOfJoing").Value);
+                spendingTimeWithCompany = DateTime.Today.Year - date.Year;
+            }
+
+            if (spendingTimeWithCompany > 5)
+            {
+                return new string[] { "High Time1", "High Time2", "High Time3", "High Time4", "High Time5" };
+            }
+            else
+            {
+                return new string[] { "value1", "value2", "value3", "value4", "value5" };
+            }
         }
     }
 }
